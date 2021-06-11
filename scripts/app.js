d1c451df8c4bf4b12778cdc27f56ecc23eb1f2da
Couapy <mael@marchand.cloud>
2021-06-11 14:34:31 +0200

    feat(style): add reveal animation

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,8 @@
 import Project from './customElements/Project.js'
 import Menu from './customElements/Menu.js'
+import revealAnimationSetup from './RevealAnimation.js'
 
 window.customElements.define('div-project', Project, { extends: 'div' })
 window.customElements.define('div-menu', Menu, { extends: 'div' })
+
+revealAnimationSetup();
