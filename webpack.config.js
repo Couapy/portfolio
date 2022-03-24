4f707006672a14e2ce0066887768193aeac8aae9
Couapy <mael@marchand.cloud>
2022-03-24 15:36:11 +0100

    feat: use local fonts

--- webpack.config.js
+++ webpack.config.js
@@ -60,6 +60,7 @@ module.exports = {
   ],
   devServer: {
     static: { directory: path.resolve(__dirname, 'build') },
+    watchFiles: ['./src/index.html'],
     compress: true,
     port: 8000,
   },
